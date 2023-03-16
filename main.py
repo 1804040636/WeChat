e0d080e77fdfa96242da224882f42d2e048e06b7
浅梦 <64389500+1804040636@users.noreply.github.com>
2023-03-17 06:53:20 +0800

    修改计算日期逻辑+8h

--- main.py
+++ main.py
@@ -114,9 +114,10 @@ def get_ciba():
 def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
+    t_localtime=localtime(time()+28800)
+    year = t_localtime.tm_year
+    month = t_localtime.tm_mon
+    day = t_localtime.tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
     week = week_list[today.isoweekday() % 7]
     # 获取在一起的日子的日期格式
